532a274a14322d4674997211ee1965142ece22de
omc32 <omc32@management-client.fritz.box>
2024-10-06 14:32:06 +0200

    Initial push of current state to github holgerpriess.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+//import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import critters from "astro-critters";
+import icon from "astro-icon";
+import { shield } from '@kindspells/astro-shield';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +13,15 @@ export default defineConfig({
     port: command === 'dev' ? 6000 : 5000
   }),
   site: "https://holgerpriess.com",
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap(), critters()]
+  integrations: [tailwind(), 
+                //image({
+                //  serviceEntryPoint: "@astrojs/image/sharp"
+                //}),
+                mdx(), 
+                sitemap(), 
+                critters(),
+                icon(),
+                shield({
+                  sri: { enableStatic: false },
+                }),]
 });
